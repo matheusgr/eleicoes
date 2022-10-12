797bafa54f8849fc5f80ac585194322b6e0a15f4
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2022-10-12 12:58:01 -0300

    Refactoring.

--- leitor_urnas.py
+++ leitor_urnas.py
@@ -1,10 +1,8 @@
 import collections
-import io
 import os
 from zipfile import ZipFile
 import random
 
-import asn1crypto.core as asn1
 import py7zr
 import numpy as np
 
@@ -15,45 +13,55 @@ import urna_classifier
 import urna_group
 
 
+def get_logejz(zip_name, myzip):
+    if not zip_name.endswith('rdv'):
+        return
+    if random.random() >= 0.01:
+        return
+    logejz_name = zip_name[:-3] + 'logjez'
+    try:
+        return myzip.open(logejz_name)
+    except KeyError:
+        return  # dont read RDV without logejz
+
+
+def get_sample(sample, logs):
+    if sample is None:
+        return logs[np.random.choice(logs.shape[0], 1)]
+    else:
+        return np.concatenate((sample, logs[np.random.choice(logs.shape[0], 1)]))    
+
+
+def read_logs(logejz_file, num_votos):
+    f_log = py7zr.SevenZipFile(logejz_file)
+    file_content = f_log.read('logd.dat')
+    return process_log(file_content['logd.dat'], num_votos)
+
+
 def process_logs(action, sampling=True):
     sample = None
-    logs = []
     for fname in os.listdir("urnas"):
         f_urna = "urnas" + os.sep + fname
-        i = 0
         with ZipFile(f_urna) as myzip:
             print("....", f_urna)
             for zip_name in myzip.namelist():
-                if not zip_name.endswith('rdv'):
+                logejz_file = get_logejz(zip_name, myzip)
+                if logejz_file is None:
                     continue
-                if random.randint(0, 100) > 1:  # sample 1% of sections
-                    continue
-                logejz_name = zip_name[:-3] + 'logjez'
-                try:
-                    my_file = myzip.open(logejz_name)
-                except:
-                    continue  # dont read RDV without logejz
                 with myzip.open(zip_name) as myfile:
                     # 13, 22, brancos/nulos, outros
                     res_presidentes, num_votos =  ler_rdv(myfile.read())
-                f_log = py7zr.SevenZipFile(my_file)
-                file_content = f_log.read('logd.dat')
-                resultados = process_log(file_content['logd.dat'], num_votos)
-                if not resultados is None:
-                    logs = resultados
+                logs = read_logs(logejz_file, num_votos)
+                if logs is not None:
                     action(logs, res_presidentes)
                     if sampling:
-                        if sample is None:
-                            sample = logs[np.random.choice(logs.shape[0], 1)]
-                        else:
-                            sample = np.concatenate((sample, logs[np.random.choice(logs.shape[0], 1)]))
-                    logs = []
+                        sample = get_sample(sample, logs)
     return sample
 
 
 # PREPARE CLUSTERING
 def prepare_clustering():
-    brcs = [urna_group.prepare(x) for x in range(2,14)]
+    brcs = [urna_group.prepare(x) for x in range(6,18)]
     prepare_classifier = lambda logs, _: [urna_group.partial_fit(brc, logs) for brc in brcs]
     sample = process_logs(prepare_classifier)
     for i, brc in enumerate(brcs):
@@ -62,9 +70,8 @@ def prepare_clustering():
 
 
 # TRAIN NEURAL NETWORK
-def train_neural():
-    sections = []
-    brc = urna_group.load('model-8.joblib')
+def train_neural(model_name):
+    brc = urna_group.load(model_name)
     clf = urna_classifier.prepare()
     test_x = []
     test_y = []
@@ -76,7 +83,7 @@ def train_neural():
         if random.randint(0,10) > 8:
             # test
             test_x.append(i_layer),
-            test_y.append(rdv[0])
+            test_y.append(rdv[1])
         else:
             urna_classifier.partial_fit(clf, [i_layer], [rdv[0]])
     process_logs(prepare_base, False)
@@ -85,5 +92,5 @@ def train_neural():
     urna_group.save(clf, 'nn')
 
 
-prepare_clustering()
-train_neural()
+#prepare_clustering()
+train_neural('model-11.joblib')
--- log.py
+++ log.py
@@ -2,7 +2,7 @@ import datetime
 
 import numpy as np
 
-def convertDate(str_date, str_time):
+def convert_date(str_time):
     return (int(str_time[0:2]) * 3600) + (int(str_time[3:5]) * 60) + int(str_time[6:8])
 
 
@@ -20,18 +20,15 @@ def process_log(lines, expected):
     i = 0
     for line in lines:
         fields  = line.split(b'\t', maxsplit=5)
-        c_date = fields[0][0:9]
         c_time = fields[0][11:20]
-        c_type = fields[1]
-        c_urna = fields[2]
         c_event = fields[3]
         c_data = fields[4]
         if c_event == b"VOTA":
             if b"Voto confirmado para " in c_data:
-                f_sec = convertDate(c_date, c_time) - convertDate(c_date, v_time)
+                f_sec = convert_date(c_time) - convert_date(v_time)
                 if i == LEN_SIZE:
                     continue  # Conselheiro digital para algumas urnas
-                result[t][i] = min(30, f_sec)/30.0
+                result[t][i] = min(10, f_sec)/10.0
                 i += 1
                 result[t][i] = min(5, c_tecl)/5.0
                 i += 1
@@ -42,17 +39,11 @@ def process_log(lines, expected):
                 result[t][i] = max(0, min(10, (int(c_time[0:2]) - 8)))/10.0  # tempo
                 i += 1
                 c_tecl = 0
-                i_time = c_time
                 v_time = c_time
-                pass
             elif b"O voto do eleitor foi computado" in c_data:
                 if i == LEN_SIZE:
                     t += 1
                 i = 0
-                e_time = c_time
-                i_time = 0
-                e_time = 0
-                pass
             elif b"Tecla indevida pressionada" in c_data:
                 c_tecl += 1
     if not t:
